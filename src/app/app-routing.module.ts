cb5c12e75b6fc7e7c1cbf49ee3b6529f7d638d1e
halamahmoud <hala.elzahlan@assesstm.com>
2021-05-14 00:39:05 +0200

    medical commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home', loadChildren: () => import('./medical/medical-data/medical-data.module').then(
+      m => m.MedicalDataModule
+    )
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
